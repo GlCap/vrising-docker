39afb76479b8fc4380832af399fac436e10b1693
Gianluca M. Caputo <me@glcap.dev>
2024-05-25 15:18:18 +0000

    feat: multiple improvements
    - add UID and GID host mapping
    - add ports environment variables
    - add data path as bind mount

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,20 @@ xvfb_wine() {
 
 mkdir -p /home/vrising/.wine/drive_c/VRisingData/Settings
 if [ -f settings/ServerGameSettings.json ]; then
+  echo "Installing ServerGameSettings..."
   cp settings/ServerGameSettings.json /home/vrising/.wine/drive_c/VRisingData/Settings/ServerGameSettings.json
 fi
 if [ -f settings/ServerHostSettings.json ]; then
+  echo "Installing ServerHostSettings..."
   cp settings/ServerHostSettings.json /home/vrising/.wine/drive_c/VRisingData/Settings/ServerHostSettings.json
 fi
 
 xvfb_wine winecfg -v win10
+echo "Starting SteamCMD to install/update game..."
 xvfb_wine steamcmd.exe +force_install_dir 'C:\VRisingServer' +login anonymous +app_update 1829350 validate +quit
+
+sleep 1
+
+echo "Starting game..."
+
 xvfb_wine VRisingServer.exe -persistentDataPath 'C:\VRisingData'
